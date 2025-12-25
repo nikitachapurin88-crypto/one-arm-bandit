b5e11da6832dacd6dea5dab517fc560b0726972e
thflsmnn <sofamalafeeva07@gmail.com>
2025-12-25 20:46:02 +0300

    pt

--- Paytable.h
+++ Paytable.h
@@ -6,14 +6,14 @@
 #include "Symbol.h"    
 class PT{
 public:
-    static constexpr mc=5;
+    static constexpr int mc=5;
     PT()=default;
     ~PT()=default;
     void zachto(Symbol s,int count,int payout);
     int skoka(Symbol s,int count) const;
     bool has(Symbol s,int count) const;
 private:
-    std::unordered_map<Symbol ,std::array<int,mc+1>> table;
+    std::unordered_map<Symbol,std::array<int,mc+1>> table;
 };
 std::shared_ptr<PT> makeDPT();
 #endif
